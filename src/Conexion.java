d0962c74c58d7b0f6bfa308ef09fdc49c7cce2b2
Pablo Rizo <prozasoconw4@gmail.com>
2020-01-17 16:34:13 -0600

    Primer commit del proyecto

---
+++ src/Conexion.java
@@ -0,0 +1,28 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class Conexion
+{
+    public Connection get_connection()
+    {
+
+        Connection connection = null;
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/mensajes_app", "root","");
+            if(connection != null)
+            {
+                System.out.println("conexion exitosa");
+            }
+        }
+        catch(SQLException | ClassNotFoundException e)
+        {
+            System.out.println(e);
+        }
+
+        return connection;
+    }
+}
